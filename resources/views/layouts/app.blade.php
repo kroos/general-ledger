cd3a53dc853b6b0c40e6894bc859549e67a3af86
Dhiauddin <dhiauddin@gmail.com>
2026-02-19 17:30:46 +0800

    -- update app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <?php
 use \Carbon\Carbon;
-
 $currentYear = Carbon::now()->year;
 ?>
 <!doctype html>
@@ -15,13 +14,13 @@ $currentYear = Carbon::now()->year;
 
 	<title>{{ config('app.name', 'Laravel') }}</title>
 
-	<link href="" type="image/x-icon" rel="icon" />
+	<link href="{{ asset('images/logo.png') }}" type="image/x-icon" rel="icon" />
 
 	<!-- Styles / Scripts -->
 	@vite(['resources/scss/app.scss', 'resources/css/app.css'])
 
 	<!-- Bootswatch Cerulean CSS -->
-	<link href="{{ URL::asset('css/bootstrap.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 	<!-- Livewire CSS -->
 
 </head>
@@ -31,7 +30,7 @@ $currentYear = Carbon::now()->year;
 	@include('layouts.navbar')
 	<!-- 1st nav end -->
 
-	<div class="container-fluid flex-fill">
+	<div class="container-fluid flex-fill d-flex flex-column">
 
 		<div class="col-sm-12 mx-auto my-2">
 			<!-- 2nd nav -->
@@ -39,42 +38,42 @@ $currentYear = Carbon::now()->year;
 			<!-- 2nd nav end -->
 		</div>
 
-		<div class="container-fluid p-1 mx-auto">
-			<div class="col-sm-12 m-0 row justify-content-between">
 
-				<div class="col-sm-2 m-0">
-					<!-- left side menu -->
-					@include('layouts.navleftside')
-					<!-- left side menu end -->
+		<div class="container-fluid p-1 mx-auto d-flex justify-content-between flex-fill">
+
+			<div class="col-sm-2 m-0 align-self-stretch">
+				<!-- left side menu -->
+				@include('layouts.navleftside')
+				<!-- left side menu end -->
+			</div>
+
+
+			<div class="col-sm-7 m-0 my-2 p-1 align-self-center">
+				<div class="col-sm-12">
+					<!-- error message -->
+					@include('layouts.messages')
+					<!-- error message end -->
 				</div>
-
-
-				<div class="col-sm-7 m-0 my-2 p-1 align-self-center">
-					<div class="col-sm-12">
-						<!-- error message -->
-						@include('layouts.messages')
-						<!-- error message end -->
-					</div>
 				<!-- content -->
 				@isset($slot)
 					<div class="tw">{{ $slot }}</div>
 				@endisset
 				@yield('content')
 				<!-- content end -->
-				</div>
-
-				<div class="col-sm-2 m-0 p-1">
-					<!-- right side info -->
-					@include('layouts.inforightside')
-					<!-- right side info end -->
-				</div>
 			</div>
+
+			<div class="col-sm-2 m-0 p-1 align-self-stretch">
+				<!-- right side info -->
+				@include('layouts.inforightside')
+				<!-- right side info end -->
+			</div>
+
 		</div>
 
 	</div>
 	<!-- footer -->
 	<div class="container m-0 mx-auto py-1 align-self-end text-center text-sm text-light-emphasis">
-		&copy; My Themes<br />Themes By Bootswatch<br />
+		&copy; {{ config('app.name', 'Laravel') }} {{ $currentYear }}<br />
 		Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
 	</div>
 	<!-- footer end -->
